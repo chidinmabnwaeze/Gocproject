c9c3140c12a48f840e9db8e0ce987a853e75f13f
chidinmabnwaeze <138997346+chidinmabnwaeze@users.noreply.github.com>
2024-05-14 11:07:25 +0100

    update

--- logistics/src/components/footer.jsx
+++ logistics/src/components/footer.jsx
@@ -1,25 +1,47 @@
-import react from "react"
+import react from "react";
 
-function Footer (){
-   return (
-   <>
-    <div className="footer">
+function Footer() {
+  return (
+    <div className="whole-footer">
+      <div className="footer">
+
+         <div className="footer1">
         <div className="footer-logo">
-        <p>Logi<span>Hub</span></p>
+          <p>
+            Logi<span>Hub</span>
+          </p>
         </div>
-        {/* <div className="sm-links">
-        <i class='bx bxl-facebook' style='color:#0a0a0a' ></i>
-        <i class='bx bxl-twitter'></i>
-        <i class='bx bxl-linkedin'></i>
-        <i class='bx bxl-instagram'></i>
-        </div> */}
+        <div className="sm-links">
+        <img className='facebook' src="./icons/"></img>
+        <img className='twitter' src="./icons/" ></img>
+        <img className='linkedin' src="./icons/" ></img>
+        <img className='instagram' src="./icons/" ></img>
+        </div>
+        </div>
 
+        <footer-links>
+          <p className="footer-head">Company</p>
+          <p className="footer-sub">Contact Us</p>
+        </footer-links>
 
-     </div>
-     <hr />
-     <small>Copyright © 2022 LogiHub. All rights reserved.</small>
-    </>
-   )
+        <footer-links>
+          <p className="footer-head">Legal</p>
+          <p className="footer-sub">Privacy Policy</p>
+          <p className="footer-sub">Terms of Service</p>
+        </footer-links>
+
+        <footer-links>
+          <p className="footer-head">Help</p>
+          <p className="footer-sub">Read our FAQs</p>
+        </footer-links>
+
+      </div>
+      <div className="copy">
+          <hr />
+          <small>Copyright © 2022 LogiHub. All rights reserved.</small>
+        </div>
+    </div>
+  );
 }
 
 export default Footer;
