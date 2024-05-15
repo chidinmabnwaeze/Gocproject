4e5097ec7eaa838cbeac7dc23c077f7e86df725b
chidinmabnwaeze <138997346+chidinmabnwaeze@users.noreply.github.com>
2024-05-15 15:54:24 +0100

    update

--- logistics/src/components/login.jsx
+++ logistics/src/components/login.jsx
@@ -6,6 +6,38 @@ function Login (){
 <div className="image-div">
     <img src={padlock} alt="" />
 </div>
+<div className="login-input">
+    <div className="welcome">
+        <h1>Welcome Back</h1>
+        <p>Lorem ipsum dolor sit amet sit amet.</p>
+    </div>
+    <div className="icons">
+        <img className="icon1" src="" alt="google" />
+        <img className="icon2" src="" alt="apple" />
+        <img className="icon3" src="" alt="facebook" />
+    </div>
+    <div className="or">
+        <hr /><span>OR</span><hr />
+    </div>
+    <form action="submit">
+        <label htmlFor="email">
+            Email
+            <input type="text" placeholder="@gmail.com"/>
+        </label>
+        <label htmlFor="password">Password
+        <input type="text" placeholder="Enter Password"/>
+        </label>
+      <div className="remember-reset">
+      <div className="remember">
+            <input type="checkbox" />
+            Remember me
+        </div>
+        <div className="reset">
+            <a href="">Reset Password</a>
+        </div>
+      </div>
+    </form>
+</div>
        </div> 
     )
 }
