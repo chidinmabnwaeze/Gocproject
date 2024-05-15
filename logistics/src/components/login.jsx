bb4c8f3432121104cb48881f848f712df5e9df71
chidinmabnwaeze <138997346+chidinmabnwaeze@users.noreply.github.com>
2024-05-15 15:40:50 +0100

    new changes

---
+++ logistics/src/components/login.jsx
@@ -0,0 +1,11 @@
+import react from "react";
+
+function Login (){
+    return(
+       <div className="login">
+<div className="image-div">
+    <img src={padlock} alt="" />
+</div>
+       </div> 
+    )
+}
