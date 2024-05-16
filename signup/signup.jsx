ebe7acb21643a3c063732a1fdc073ab63374e6e6
chidinmabnwaeze <138997346+chidinmabnwaeze@users.noreply.github.com>
2024-05-16 03:59:14 +0100

    update

--- signup/signup.jsx
+++ signup/signup.jsx
@@ -1,7 +1,8 @@
 import react from "react";
+import Header from "../logistics/src/components/header";
 
 export default function Signup (){
     <div className="sign-up">
-        
+       <Header/> 
     </div>
 }
