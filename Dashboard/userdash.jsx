f91ace521036f42fb604186fc066bd0d294d61ec
chidinmabnwaeze <138997346+chidinmabnwaeze@users.noreply.github.com>
2024-05-19 22:00:18 +0100

    update

--- Dashboard/userdash.jsx
+++ Dashboard/userdash.jsx
@@ -1,9 +1,15 @@
 import react from "react";
 
-export default function Dashboard (){
-    return (
-        <div>
-            
+export default function Dashboard() {
+  return (
+    <div className="dashboard">
+      <div className="actions">
+
+        <div className="action-card">
+            <img src="" alt="" />
         </div>
-    )
+        
+      </div>
+    </div>
+  );
 }
