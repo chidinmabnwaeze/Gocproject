3391b79c69e5c739e0899e6382c416dcfe42eb89
chidinmabnwaeze <138997346+chidinmabnwaeze@users.noreply.github.com>
2024-05-19 22:04:38 +0100

    update

--- Dashboard/userdash.jsx
+++ Dashboard/userdash.jsx
@@ -7,8 +7,15 @@ export default function Dashboard() {
 
         <div className="action-card">
             <img src="" alt="" />
+
+            <div className="action-card-text">
+                <h3 className="action-head">Parcel Delivery</h3>
+                <p className="action-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Consectetur autem corrupti soluta! Minima fuga repellat dolorem neque reiciendis. 
+                    Nulla delectus odio magnam aut, quam rem corrupti reiciendis optio necessitatibus et.</p>
+            </div>
         </div>
-        
+
       </div>
     </div>
   );
