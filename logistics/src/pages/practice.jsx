b1f1eb0c25dff3a6fcb28c6c9324f3834b6f1526
chidinmabnwaeze <138997346+chidinmabnwaeze@users.noreply.github.com>
2024-05-29 12:27:18 +0100

    updates

---
+++ logistics/src/pages/practice.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { useState } from 'react'
+
+const practice = () => {
+ const [items, setItems]= useState([
+    {
+        id : 1,
+        checked :false,
+        item: "one bag of rice"
+    },
+    {
+        id : 2,
+        checked :false,
+        item: "one bag of beans"
+    },
+    {
+        id : 3,
+        checked :false,
+        item: "one bag of garri"
+    }
+
+ ]);
+ 
+ 
+    return (
+    <main>
+<ul>
+    {items.map((item)=>{
+<li className='item' key={item.id}>
+    <input type="checkbox" 
+    name="input" 
+    id="" 
+    />
+
+    <span>{items.item}</span>
+</li>
+    })}
+</ul>
+    </main>
+  )
+}
+
+export default practice
