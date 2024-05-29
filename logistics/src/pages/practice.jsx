3e057b013910bc43013c2a7f7185628859bc3649
chidinmabnwaeze <138997346+chidinmabnwaeze@users.noreply.github.com>
2024-05-29 13:09:04 +0100

    changes

--- logistics/src/pages/practice.jsx
+++ logistics/src/pages/practice.jsx
@@ -1,25 +1,26 @@
 import React from "react";
 import { useState } from "react";
 
-const [items, setItems] = useState([
-    {
-      id: 1,
-      checked: false,
-      item: "one bag of rice"
-    },
-    {
-      id: 2,
-      checked: false,
-      item: "one bag of beans"
-    },
-    {
-      id: 3,
-      checked: false,
-      item: "one bag of garri"
-    }
-  ]);
-
 const Practice = () => {
+    const [items, setItems] = useState([
+        {
+          id: 1,
+          checked: false,
+          item: "one bag of rice"
+        },
+        {
+          id: 2,
+          checked: false,
+          item: "one bag of beans"
+        },
+        {
+          id: 3,
+          checked: false,
+          item: "one bag of garri"
+        }
+      ]);
+
+
   return (
     <main>
       <ul>
