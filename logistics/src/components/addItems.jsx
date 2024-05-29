5ea3e7ef59e4f8bc28de33014b4d5aad216c80a6
chidinmabnwaeze <138997346+chidinmabnwaeze@users.noreply.github.com>
2024-05-29 16:13:01 +0100

    modifications

--- logistics/src/components/addItems.jsx
+++ logistics/src/components/addItems.jsx
@@ -1,21 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const addItems = () => {
   return (
-    <form className='addForm'>
-<label htmlFor="addItem">Add Item</label>
-<input
-autoFocus
- type="text" 
- id ="addItem"
- placeholder='Add Item'
- required
-
-/>
+    <form className="addForm">
+      <label htmlFor="addItem">Add Item</label>
+      <input
+        autoFocus
+        type="text"
+        id="addItem"
+        placeholder="Add Item"
+        required
+      />
     </form>
+  );
+};
 
-
-  )
-}
-
-export default addItems
+export default addItems;
