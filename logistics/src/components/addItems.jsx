d88f9bf811d32117570737ed5549eb5a6643e0d9
chidinmabnwaeze <138997346+chidinmabnwaeze@users.noreply.github.com>
2024-05-30 23:57:31 +0100

    changes

--- logistics/src/components/addItems.jsx
+++ logistics/src/components/addItems.jsx
@@ -2,15 +2,7 @@ import React from "react";
 
 const addItems = () => {
   return (
-    <form className="addForm">
-      <label htmlFor="addItem">Add Item</label>
-      <input
-        autoFocus
-        type="text"
-        id="addItem"
-        placeholder="Add Item"
-        required
-      />
+   
       <button
       type="submit"
       aria-label="Add Item"
